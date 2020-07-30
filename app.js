8fcbde610cbb0d37cf20becf81dc2a3332760676
Alex. Danicic <alex.danicic@gmail.com>
2020-07-30 12:04:45 +0100

    js done

---
+++ app.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  //   console.log(sidebar.classList);
+  // long way
+  /*
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
+  */
+
+  //   shorter way
+  sidebar.classList.toggle("show-sidebar");
+});
+
+// close the sidebar with 'X' when it is opened
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
